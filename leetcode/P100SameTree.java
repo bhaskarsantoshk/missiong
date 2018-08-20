6300972a57f158e0de398dab8301789d45141ffd
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-08-21 00:04:39 +0530

    leetcode/P100SameTree.java

---
+++ leetcode/P100SameTree.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class P100SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if( p == null && q == null ){
+            return true;
+        }
+        if( p == null || q == null ){
+            return false;
+        }
+
+        return p.val == q.val && isSameTree(p.left , q.left) && isSameTree(p.right , q.right);
+    }
+
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){
+            this.val = x;
+        }
+    }
+}
