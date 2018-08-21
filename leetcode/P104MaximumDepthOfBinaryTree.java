3cd0fcd2b6b4ef03794d321975177b971152df7c
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-08-21 10:51:11 +0530

    leetcode/P104MaximumDepthOfBinaryTree.java

---
+++ leetcode/P104MaximumDepthOfBinaryTree.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class P104MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if ( root == null ){
+            return 0;
+        }
+        int leftDepth = maxDepth( root.left );
+        int rightDepth = maxDepth( root.right );
+        int maxDepth = Math.max( leftDepth , rightDepth ) + 1;
+        return maxDepth;
+    }
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode (int x){
+            this.val=x;
+        }
+    }
+}
