12ea5131df22c18d15db4ab2deacf6034c77b70e
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-08-22 21:52:40 +0530

    leetcode/P141LinkedListCycle.java

---
+++ leetcode/P141LinkedListCycle.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class P141LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if( head == null || head.next == null ){
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while ( slow != fast ){
+            if( fast == null || fast.next == null ){
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+    class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){
+            val=x;
+        }
+    }
+}
