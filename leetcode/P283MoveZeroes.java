0b4c434beff7df016f52d66106a6816227b20501
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-08-27 23:22:03 +0530

    leetcode/P283MoveZeroes.java

---
+++ leetcode/P283MoveZeroes.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class P283MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int index=0;
+        for( int i = 0 ; i < nums.length ; i++ ){
+            if(nums[i]!=0){
+                nums[index]=nums[i];
+                if( i != index ){
+                    nums[i]=0;
+                }
+                index++;
+            }
+        }
+    }
+}
