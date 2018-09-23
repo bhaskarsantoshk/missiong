2c225e93e92f43ad9b24febfcc8399a88c8b9047
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-09-23 23:24:20 +0530

    leetcode/P412FizzBuzz.java

---
+++ leetcode/P412FizzBuzz.java
@@ -0,0 +1,27 @@
+package leetcode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class P412FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        int i=1;
+        while(i<=n) {
+            if(i%3==0 && i%5==0) {
+                list.add("FizzBuzz");
+            }
+            else if(i%3==0) {
+                list.add("Fizz");
+            }
+            else if(i%5==0){
+                list.add("Buzz");
+            }
+            else{
+                list.add(i+"");
+            }
+            i++;
+        }
+        return list;
+    }
+}
+}
