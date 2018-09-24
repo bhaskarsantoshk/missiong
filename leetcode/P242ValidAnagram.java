edba1a00ad1c8369d712cf04f441b21e0f608aa8
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-09-24 23:02:57 +0530

    leetcode/P242ValidAnagram.java

---
+++ leetcode/P242ValidAnagram.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class P242ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+       char a[]=s.toCharArray();
+       char b[]=t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        for (int i=0;i<a.length;i++){
+            if(a[i]!=b[i]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
