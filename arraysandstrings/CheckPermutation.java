a2d0caa34f608930ba06bee9bf2f263c519ac7e5
santosh.bhaskar <bhaskrsantoshk@gmail.com>
2018-09-25 23:02:08 +0530

    arraysandstrings

---
+++ arraysandstrings/CheckPermutation.java
@@ -0,0 +1,28 @@
+package com.ctci.arraysandstrings;
+
+public class CheckPermutation {
+
+    public static void main(String []args){
+        String s1="GoogleFacebookMicroSoft";
+        String s2="FacebookMicroSoftGoogle";
+
+        System.out.println(checkPermutation(s1,s2));
+    }
+
+    public static boolean checkPermutation(String s1, String s2){
+         s1=s1.toLowerCase();
+         s2=s2.toLowerCase();
+        int a[]=new int[26];
+        int b[]=new int[26];
+        for(int i=0;i<s1.length();i++){
+            a[s1.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s2.length();i++){
+            b[s2.charAt(i)-'a']++;
+        }
+        for(int i=0;i<26;i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+}
---
+++ arraysandstrings/PalindromePermutation.java
@@ -0,0 +1,33 @@
+package com.ctci.arraysandstrings;
+
+public class PalindromePermutation {
+
+    public static void main(String [] args){
+        String s="tact coa";
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s){
+        int a[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c!=' ') {
+                if (a[c - 'a'] == 1) {
+                    a[c - 'a']--;
+                } else {
+                    a[c - 'a']++;
+                }
+            }
+        }
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(a[i]==0){
+                count++;
+            }
+        }
+        if(count==26 || count==25){
+            return true;
+        }
+        return false;
+    }
+}
