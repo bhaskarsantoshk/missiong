d6c4ef3a3a5ed19bc688500283b9964af780e8cd
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-10-08 22:30:04 +0530

    smartinterviewsportal

---
+++ smartinterviewsportal/SubSetsOfAnArray.java
@@ -0,0 +1,10 @@
+package smartinterviewsportal;
+
+public class SubSetsOfAnArray {
+    public static void main(String[] args) {
+        String[] str = {"3", "5", "15"};
+        //powerSet(str);
+    }
+
+
+}
---
+++ smartinterviewsportal/TwoSetBits.java
@@ -0,0 +1,39 @@
+package smartinterviewsportal;
+
+import java.util.Scanner;
+
+public class TwoSetBits {
+
+    static int powerOfTwo[]= new int[200];
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+//        int t=in.nextInt();
+        int result[]= new int[100001];
+
+        for(int i=0;i<powerOfTwo.length;i++){
+            powerOfTwo[i]= powOfTwo(2,i);
+            System.out.println(powerOfTwo[i]);
+        }
+
+        System.out.println(powerOfTwo[100]);
+        int x=1;
+
+
+    }
+
+    static int powOfTwo(int x, int i){
+        if(powerOfTwo[i]!=0){
+            return powerOfTwo[i];
+        }
+
+        else{
+            int temp= powOfTwo(2, i/2);
+            if (i%2==0){
+                return ( (temp % 1000000007) * (temp % 1000000007) )%1000000007;
+            }
+            else{
+                return ( (((temp % 1000000007) * (temp % 1000000007) )%1000000007) * 2)% 1000000007;
+            }
+        }
+    }
+}
