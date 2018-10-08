94b9f91389189cb0884b3f126f8c30d049684d07
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-10-09 00:43:33 +0530

    multithreading/Deadlocking.java

---
+++ multithreading/Deadlocking.java
@@ -0,0 +1,14 @@
+package multithreading;
+
+public class Deadlocking {
+    public static void main(String[] args){
+        try {
+            System.out.println("You don't believe in me unless you know me well");
+            Thread.currentThread().join();
+            System.out.println("I won't let you know me well unless you start believing in me");
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
