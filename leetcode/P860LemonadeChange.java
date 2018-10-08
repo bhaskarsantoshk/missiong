13aac038ff219b19c38bb4d094e645e440f160ac
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-10-08 22:28:22 +0530

    leetcode/P860LemonadeChange.java

---
+++ leetcode/P860LemonadeChange.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+public class P860LemonadeChange {
+    public boolean lemonadeChange(int[] bills) {
+        int change[]= new int[3];
+
+        for (int i=0;i<bills.length;i++){
+            if(bills[i]==5){
+                change[0]++;
+            }
+            else if(bills[i]==10){
+                if(change[0]>0){
+                    change[0]--;
+                    change[1]++;
+                }
+                else{
+                    return false;
+                }
+            }
+
+            else if(bills[i]==20){
+                if(change[1]> 0 && change[0]>0){
+                    change[1]--;
+                    change[0]--;
+                }
+                else if(change[0]>3){
+                    change[0]-=3;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
+    }
+}
