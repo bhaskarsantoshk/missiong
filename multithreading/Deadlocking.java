4598c73bfc1ff5ce1e704db6b97d4605c116d16f
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-10-09 01:05:33 +0530

    multithreading/Deadlocking.java

--- multithreading/Deadlocking.java
+++ multithreading/Deadlocking.java
@@ -4,12 +4,14 @@ public class Deadlocking {
     public static void main(String[] args){
         try {
             System.out.println(
-   "You don't believe in me unless you know me well"
+"You don't believe in me " +
+        "unless you know me well"
             );
             Thread.currentThread().join();
 
             System.out.println(
-   "I won't let you know me well unless you start believing in me"
+"I won't let you know me well " +
+        "unless you start believing in me"
             );
         }
         catch (InterruptedException e){
