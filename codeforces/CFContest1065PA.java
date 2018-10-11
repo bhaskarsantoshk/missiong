5cff9732e0c5af99b5f502d2924dbd3219c1a069
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-10-11 22:16:16 +0530

    codeforces/CFContest1065PA.java

---
+++ codeforces/CFContest1065PA.java
@@ -0,0 +1,37 @@
+package codeforces;
+import java.util.*;
+
+public class CFContest1065PA {
+    public static void main(String [] args){
+
+        Scanner in= new Scanner(System.in);
+
+        int t=in.nextInt();
+
+        while(t!=0){
+            long s= in.nextLong();
+            long a= in.nextLong();
+            long b= in.nextLong();
+            long c= in.nextLong();
+
+            long x= a*c;
+            long y= s/x;
+
+            long z= a * y;
+            long k= b * y;
+
+            long temp = z+k;
+
+            long rem = s % x;
+
+            long f= rem/c;
+
+
+
+            System.out.println(f+temp);
+
+            t--;
+        }
+
+    }
+}
