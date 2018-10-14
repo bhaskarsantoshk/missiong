a30a454a220151aace9c2669d7c37f69cfade8c8
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2018-10-14 19:56:34 +0530

    Refactored

--- codeforces/CFContest1058PA.java
+++ codeforces/CFContest1058PA.java
@@ -1,16 +1,11 @@
 package codeforces;
-
 import java.util.*;
 
 public class CFContest1058PA {
     public static void main(String args[]){
-
         Scanner in = new Scanner(System.in);
-
         int n=in.nextInt();
-
         int sum=0;
-
         while( n!=0){
             sum+=in.nextInt();
             n--;
@@ -19,9 +14,6 @@ public class CFContest1058PA {
         if(sum!=0){
             s="HARD";
         }
-
         System.out.println(s);
-
     }
-
 }
