a4de58f99cba20c511cf845bd39b114eb208d96d
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2018-10-14 20:01:32 +0530

    Refactor

--- codeforces/CFContest1058PA.java
+++ codeforces/CFContest1058PA.java
@@ -2,18 +2,18 @@ package codeforces;
 import java.util.*;
 
 public class CFContest1058PA {
-    public static void main(String args[]){
-        Scanner in = new Scanner(System.in);
-        int n=in.nextInt();
-        int sum=0;
-        while( n!=0){
-            sum+=in.nextInt();
-            n--;
-        }
-        String s="EASY";
-        if(sum!=0){
-            s="HARD";
-        }
-        System.out.println(s);
-    }
+ public static void main(String args[]) {
+  Scanner in = new Scanner(System.in);
+  int n = in .nextInt();
+  int sum = 0;
+  while (n != 0) {
+   sum += in .nextInt();
+   n--;
+  }
+  String s = "EASY";
+  if (sum != 0) {
+   s = "HARD";
+  }
+  System.out.println(s);
+ }
 }
