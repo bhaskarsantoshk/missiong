ce463666e4f9e86d2e37ea78049d35d198db4d3e
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2018-10-14 19:59:59 +0530

    Refactor

--- codeforces/CFContest1065PA.java
+++ codeforces/CFContest1065PA.java
@@ -2,36 +2,36 @@ package codeforces;
 import java.util.*;
 
 public class CFContest1065PA {
-    public static void main(String [] args){
+ public static void main(String[] args) {
 
-        Scanner in= new Scanner(System.in);
+  Scanner in = new Scanner(System.in);
 
-        int t=in.nextInt();
+  int t = in .nextInt();
 
-        while(t!=0){
-            long s= in.nextLong();
-            long a= in.nextLong();
-            long b= in.nextLong();
-            long c= in.nextLong();
+  while (t != 0) {
+   long s = in .nextLong();
+   long a = in .nextLong();
+   long b = in .nextLong();
+   long c = in .nextLong();
 
-            long x= a*c;
-            long y= s/x;
+   long x = a * c;
+   long y = s / x;
 
-            long z= a * y;
-            long k= b * y;
+   long z = a * y;
+   long k = b * y;
 
-            long temp = z+k;
+   long temp = z + k;
 
-            long rem = s % x;
+   long rem = s % x;
 
-            long f= rem/c;
+   long f = rem / c;
 
 
 
-            System.out.println(f+temp);
+   System.out.println(f + temp);
 
-            t--;
-        }
+   t--;
+  }
 
-    }
+ }
 }
