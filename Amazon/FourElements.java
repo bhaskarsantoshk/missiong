a9c4a91eba22e7ef224655b82bfc3a25975bae71
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2018-10-27 16:47:00 +0530

    Update FourElements.java

--- Amazon/FourElements.java
+++ Amazon/FourElements.java
@@ -3,7 +3,6 @@ package Amazon;
 Given an array of integers, find a combination of four elements in the array whose sum is equal to a given value X.
 Input:
 First line consists of T test cases. First line of every test case consists of an integer N, denoting the number of elements in an array. Second line consists of N spaced array elements. Third line of every test case X.
-
 Output:
 Single line output, print 1 if combination is found else 0.
 
