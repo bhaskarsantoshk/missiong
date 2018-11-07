6796a9dcc4b9d19165f34ce6373dbf5c4f8ef784
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2018-11-07 22:35:47 +0530

    Update CFContest1058PA.java

--- codeforces/CFContest1058PA.java
+++ codeforces/CFContest1058PA.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class CFContest1058PA {
  public static void main(String args[]) {
   Scanner in = new Scanner(System.in);
+  
   int n = in .nextInt();
   int sum = 0;
   while (n != 0) {
