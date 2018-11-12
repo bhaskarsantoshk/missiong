3274c189487fbc73789aa9beddacc9f768c5e52c
SantoshBhaskarK <bhaskarsantoshk@gmail.com>
2018-11-12 23:13:09 +0530

    Update KadaneAlgorithm.java

--- Amazon/KadaneAlgorithm.java
+++ Amazon/KadaneAlgorithm.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
 
 public class KadaneAlgorithm {
  public static void main(String[] args) {
+  
   Scanner in = new Scanner(System.in);
   int t = in .nextInt();
   while (t != 0) {
