573ed54442c9d06d529b7b949ba94be361286e38
Santosh Bhaskar <bhaskarsantoshk@gmail.com>
2018-11-16 23:01:11 +0530

    favoriteportal/Interleavings.java

---
+++ favoriteportal/Interleavings.java
@@ -0,0 +1,30 @@
+package missiong.favoriteportal;
+
+import java.util.Scanner;
+
+public class Interleavings {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+
+        while (t!=0){
+            String s= in.nextLine();
+
+            String[] str= s.split(" ");
+
+            Interleavings obj= new Interleavings();
+            obj.printInterleavings(str[0], str[1]);
+
+
+            t--;
+        }
+    }
+
+    private void printInterleavings(String s, String t) {
+        // bn zh
+        //bnzh, bzhn, bznh, zbhn, zbnh, zhbn
+        //0 1 2 3 4 5 6 7 8 9
+        //a b c d e f g h i j
+        
+    }
+}
