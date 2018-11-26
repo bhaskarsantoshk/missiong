66ac87131066d159365a32bde0d97c5a464388a0
santosh.bhaskar <bhaskarsantoshk@kony.com>
2018-11-26 22:53:14 +0530

    leetcode/P543DiameterOfBinaryTree.java

--- leetcode/P543DiameterOfBinaryTree.java
+++ leetcode/P543DiameterOfBinaryTree.java
@@ -1,22 +1,28 @@
-package leetcode;
+package missiong.leetcode;
 
 public class P543DiameterOfBinaryTree {
-    int max = 0;
-    public int diameterOfBinaryTree(TreeNode root){
+    static int max = 0;
+    public static int diameterOfBinaryTree(TreeNode root){
         maxDepth(root);
         return max;
     }
 
-    private int maxDepth(TreeNode root){
+    public static void main(String [] args){
+        TreeNode node = new TreeNode(1);
+        node.right = new TreeNode(2);
+        node.right.right = new TreeNode(3);
+        node.right.right.right= new TreeNode(4);
+        node.right.right.right.right= new TreeNode(5);
+        System.out.println(diameterOfBinaryTree(node)+1);
+    }
+
+    private static int maxDepth(TreeNode root){
         if ( root == null ){
             return 0;
         }
-
         int left = maxDepth ( root.left );
         int right = maxDepth ( root.right );
-
         max = Math.max( max , left + right );
-
         return Math.max ( left , right) + 1 ;
     }
 
