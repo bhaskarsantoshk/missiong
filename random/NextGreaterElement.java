69d5054cd4e3aabf916198b7fede4e03fd098edf
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-11-27 22:11:32 +0530

    random/NextGreaterElement.java

---
+++ random/NextGreaterElement.java
@@ -0,0 +1,18 @@
+package missiong.random;
+
+public class NextGreaterElement {
+    public static void main(String[] args){
+        int a[]= {1,2,3,4,5};
+        int b[]= {5,4,3,2,1};
+        int c[]= {2,3,1,4,5};
+
+        nextGreaterElements(a);
+        nextGreaterElements(b);
+        nextGreaterElements(c);
+
+    }
+
+    private static void nextGreaterElements(int[] a) {
+        
+    }
+}
