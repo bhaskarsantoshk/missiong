29df285da96cc607c1120428806a1930a4f29e0f
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-11-29 23:16:23 +0530

    random/BSTfromLinkedList.java

--- random/BSTfromLinkedList.java
+++ random/BSTfromLinkedList.java
@@ -4,4 +4,20 @@ public class BSTfromLinkedList {
     public static void main(String[] args){
 
     }
+
+    public static Node constructBST(Node head){
+        Node root = head;
+        //constrcut a BST
+        return root;
+    }
+
+    static class Node{
+        int data;
+        Node prev;
+        Node next;
+
+        Node (int data){
+            this.data = data;
+        }
+    }
 }
