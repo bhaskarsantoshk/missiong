382029a82f3142d4377fdc3b2fa1253dc1c0c713
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-11-30 23:59:13 +0530

    random/ReverseAStackUsingRecursion.java

--- random/ReverseAStackUsingRecursion.java
+++ random/ReverseAStackUsingRecursion.java
@@ -3,9 +3,12 @@ package missiong.random;
 import java.util.Stack;
 
 public class ReverseAStackUsingRecursion {
-    Stack<Integer> stack = new Stack<>();
+    static Stack<Integer> stack = new Stack<>();
     public static void main(String[] args){
+        createAStack(stack);
+    }
 
+    private static void createAStack(Stack<Integer> stack) {
     }
 
     //reverse a Stack
