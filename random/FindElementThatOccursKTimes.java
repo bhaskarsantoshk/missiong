a9ae2d2694868f79c242ccadc3f7716edb7b7662
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-12-01 23:16:32 +0530

    random/FindElementThatOccursKTimes.java

--- random/FindElementThatOccursKTimes.java
+++ random/FindElementThatOccursKTimes.java
@@ -1,5 +1,7 @@
 package missiong.random;
 
+import java.util.HashMap;
+
 public class FindElementThatOccursKTimes {
     public static void main(String[] args){
         int a[]={1, 7, 4, 3, 4, 8, 7};
@@ -8,6 +10,18 @@ public class FindElementThatOccursKTimes {
     }
 
     private static int elementThatOccursKTimes(int[] a, int k) {
-        return 0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < a.length; i++) {
+            if (hm.keySet().contains(a[i])) {
+                hm.put(a[i], hm.get(a[i]) + 1);
+            } else {
+                hm.put(a[i], 1);
+            }
+            if (hm.get(a[i]) == k) {
+                return a[i];
+            }
+
+        }
+        return -1;
     }
 }
