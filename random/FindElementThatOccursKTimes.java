e19903699873b9e6e68f3e0b7bb75c48b8b0023e
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-12-01 23:07:08 +0530

    random/FindElementThatOccursKTimes.java

--- random/FindElementThatOccursKTimes.java
+++ random/FindElementThatOccursKTimes.java
@@ -2,6 +2,12 @@ package missiong.random;
 
 public class FindElementThatOccursKTimes {
     public static void main(String[] args){
-        
+        int a[]={1, 7, 4, 3, 4, 8, 7};
+        int k= 2;
+        System.out.println(elementThatOccursKTimes(a,k));
+    }
+
+    private static int elementThatOccursKTimes(int[] a, int k) {
+        return 0;
     }
 }
