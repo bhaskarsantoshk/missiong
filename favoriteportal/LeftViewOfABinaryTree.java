cd17ec9ed790e98d510852c08a67a657018b19ee
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-12-04 23:41:08 +0530

    favoriteportal/LeftViewOfABinaryTree.java

--- favoriteportal/LeftViewOfABinaryTree.java
+++ favoriteportal/LeftViewOfABinaryTree.java
@@ -3,13 +3,15 @@ package missiong.favoriteportal;
 import java.util.Scanner;
 
 public class LeftViewOfABinaryTree {
+    static TreeNode root = null;
+    static int max_level = 0;
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int t= in.nextInt();
         while (t!=0){
             int n = in.nextInt();
-            TreeNode root = null;
-            max = 1;
+            root = null;
+            max_level = 0;
             while (n!=0){
                 int val = in.nextInt();
                 root = insert(root, val);
@@ -19,10 +21,9 @@ public class LeftViewOfABinaryTree {
             t--;
         }
     }
-
     private static void leftViewOfATree(TreeNode root) {
-    }
 
+    }
     public static TreeNode insert(TreeNode root, int val){
         if (root == null ){
             root = new TreeNode(val);
