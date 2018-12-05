1c73a2265307709d947884e0813b893d4809ed3a
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-12-05 23:36:31 +0530

    random/RemoveAllTheOccurencesOfAGivenKeyFromDoublyLinkedList.java

--- random/RemoveAllTheOccurencesOfAGivenKeyFromDoublyLinkedList.java
+++ random/RemoveAllTheOccurencesOfAGivenKeyFromDoublyLinkedList.java
@@ -8,15 +8,23 @@ public class RemoveAllTheOccurencesOfAGivenKeyFromDoublyLinkedList {
 
     private static void constructLinkedList(LinkedListNode head) {
         head = new LinkedListNode(2);
+        head.prev = null;
         head.next = new LinkedListNode(2);
+        head.next.prev = head;
         head.next.next = new LinkedListNode(10);
+        head.next.next.prev = head.next;
         head.next.next.next = new LinkedListNode(8);
+        head.next.next.next.prev = head.next.next;
         head.next.next.next.next = new LinkedListNode(4);
+        head.next.next.next.next.prev = head.next.next.next;
         head.next.next.next.next.next = new LinkedListNode(2);
+        head.next.next.next.next.next.prev = head.next.next.next.next;
         head.next.next.next.next.next.next = new LinkedListNode(5);
+        head.next.next.next.next.next.next.prev = head.next.next.next.next.next;
         head.next.next.next.next.next.next.next = new LinkedListNode(2);
+        head.next.next.next.next.next.next.next.prev = head.next.next.next.next.next.next;
         LinkedListNode temp = head;
-        
+
     }
 
     static class LinkedListNode{
