a2eb38d0e24b7d00b38203b02d01fb81b97e6dec
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-12-20 23:54:18 +0530

    leetcode/P121BestTimeToBuyAndSellStock.java

--- leetcode/P121BestTimeToBuyAndSellStock.java
+++ leetcode/P121BestTimeToBuyAndSellStock.java
@@ -1,17 +1,6 @@
 package leetcode;
 
 public class P121BestTimeToBuyAndSellStock {
-//    public int maxProfit(int[] prices) {
-//        int max=0;
-//        for(int i = prices.length-1 ; i > 0 ; i--){
-//            for(int j= i-1 ; j >= 0 ; j--){
-//                int temp = prices[i] - prices [j];
-//                max = Math.max( max , temp );
-//            }
-//        }
-//
-//        return max;
-//    }
 
     public int maxProfit(int[] prices){
         int minprice= Integer.MAX_VALUE;
