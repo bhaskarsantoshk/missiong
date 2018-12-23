bc1a16943ef5bcd120b33f504c9c7871d52b47c4
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2018-12-23 23:51:01 +0530

    interviewbit/MinStepsInInfiniteGrid.java

--- interviewbit/MinStepsInInfiniteGrid.java
+++ interviewbit/MinStepsInInfiniteGrid.java
@@ -6,6 +6,7 @@ public class MinStepsInInfiniteGrid {
     public int coverPoints(ArrayList<Integer> A, ArrayList<Integer> B) {
         int distance = 0;
         //Starting Point
+        
         int x= A.get(0);
         int y=B.get(0);
         for(int i=1;i<A.size();i++){
