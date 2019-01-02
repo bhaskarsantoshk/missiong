9c3caac218bfb53ac4f51c1fcaa3628d181b26c4
santosh.bhaskar <santosh.bhaskar@kony.com>
2019-01-02 21:48:11 +0530

    interviewbit/Power.java

---
+++ interviewbit/Power.java
@@ -0,0 +1,21 @@
+package missiong.interviewbit;
+
+import java.math.BigInteger;
+
+public class Power {
+    public int power(String A) {
+        BigInteger a = new BigInteger(A);
+        if(a.equals(BigInteger.ONE) || a.equals(BigInteger.ZERO)){
+            return 0;
+        }
+
+        for(int i=0;i<a.bitLength();i++){
+            if(a.testBit(i)){
+                return 0;
+            }
+        }
+
+        return a.testBit(a.bitLength()-1)?1:0;
+
+    }
+}
