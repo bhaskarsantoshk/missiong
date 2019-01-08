e564502e0db86facce00d9f500006a49c9f0ea65
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-01-08 19:16:52 +0530

    Amazon/FourElements.java

--- Amazon/FourElements.java
+++ Amazon/FourElements.java
@@ -1,4 +1,47 @@
-package Amazon;
+package missiong.Amazon;
+
+import java.util.*;
+
+public class FourElements {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t != 0) {
+            int n = in.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = in.nextInt();
+            }
+            int sum = in.nextInt();
+            Arrays.sort(a);
+            System.out.println(isSumPossible(a, n, sum));
+            t--;
+        }
+    }
+
+    static int isSumPossible(int[] a, int n, int sum) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int minIndex = j + 1;
+                int maxIndex = n - 1;
+                while (minIndex < maxIndex) {
+                    int total = a[i] + a[j] + a[minIndex] + a[maxIndex];
+                    if (total == sum) {
+                        return 1;
+                    } else if (total < sum) {
+                        minIndex++;
+                    } else {
+                        maxIndex--;
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+}
+
+
 /*
 Given an array of integers, find a combination of four elements in the array whose sum is equal to a given value X.
 Input:
@@ -17,58 +60,3 @@ Input:
 Output:
 1
  */
-
-
-
-
-
-
-
-
-
-
-
-
-
-import java.util.*;
-
-public class FourElements {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        while(t!=0){
-            int n=in.nextInt();
-            int a[]= new int[n];
-            for(int i=0;i<n;i++){
-                a[i]=in.nextInt();
-            }
-            
-            int sum=in.nextInt();
-            Arrays.sort(a);
-            System.out.println(isSumPossible(a,n,sum));
-            t--;
-        }
-    }
-    static int isSumPossible(int[] a, int n, int sum){
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                int minIndex = j+1;
-                int maxIndex = n-1;
-                while (minIndex < maxIndex ){
-                    int total= a[i]+a[j]+a[minIndex]+a[maxIndex];
-                    if( total == sum ){
-                        return 1;
-                    }
-                    else if(total < sum ){
-                        minIndex++;
-                    }
-                    else{
-                        maxIndex--;
-                    }
-                }
-            }
-        }
-
-        return 0;
-    }
-}
--- arraysandstrings/Main.java
+++
@@ -1,8 +0,0 @@
-package com.ctci.arraysandstrings;
-
-public class Main {
-
-    public static void main(String[] args) {
-	
-    }
-}
