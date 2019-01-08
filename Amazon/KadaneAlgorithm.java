6bbba6e2c2de552c23419c152de40c489194decf
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-01-08 19:17:02 +0530

    Amazon/KadaneAlgorithm.java

--- Amazon/KadaneAlgorithm.java
+++ Amazon/KadaneAlgorithm.java
@@ -1,4 +1,33 @@
-package Amazon;
+package missiong.Amazon;
+
+import java.util.Scanner;
+
+public class KadaneAlgorithm {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t != 0) {
+            int n = in.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = in.nextInt();
+            }
+            maximumContigiousSum(a, n);
+            t--;
+        }
+    }
+
+    static void maximumContigiousSum(int[] a, int n) {
+        int localMaxSum = a[0];
+        int globalMaxSum = a[0];
+        for (int i = 1; i < n; i++) {
+            localMaxSum = Math.max(localMaxSum + a[i], a[i]);
+            globalMaxSum = Math.max(localMaxSum, globalMaxSum);
+        }
+        System.out.println(globalMaxSum);
+    }
+}
+
 /*
 Given an array containing both negative and positive integers. Find the contiguous sub-array with maximum sum.
 
@@ -8,29 +37,3 @@ The first line of input contains an integer T denoting the number of test cases.
 Output:
 Print the maximum sum of the contiguous sub-array in a separate line for each test case.
 */
-import java.util.Scanner;
-public class KadaneAlgorithm {
- public static void main(String[] args) {
-  Scanner in = new Scanner(System.in);
-  int t = in .nextInt();
-  while (t != 0) {
-   int n = in .nextInt();
-   int a[] = new int[n];
-   for (int i = 0; i < n; i++) {
-    a[i] = in .nextInt();
-    
-   }
-   maximumContigiousSum(a, n);
-   t--;
-  }
- }
- static void maximumContigiousSum(int[] a, int n) {
-  int localMaxSum = a[0];
-  int globalMaxSum = a[0];
-  for (int i = 1; i < n; i++) {
-   localMaxSum = Math.max(localMaxSum + a[i], a[i]);
-   globalMaxSum = Math.max(localMaxSum, globalMaxSum);
-  }
-  System.out.println(globalMaxSum);
- }
-}
