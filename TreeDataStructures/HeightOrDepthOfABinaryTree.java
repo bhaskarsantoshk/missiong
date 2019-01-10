8499182a16a92281fd627c4d925a34c298403523
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-01-11 00:22:54 +0530

    TreeDataStructures/HeightOrDepthOfABinaryTree.java

---
+++ TreeDataStructures/HeightOrDepthOfABinaryTree.java
@@ -0,0 +1,13 @@
+package missiong.TreeDataStructures;
+
+public class HeightOrDepthOfABinaryTree {
+    public int maxDepthRecursive(BinaryTreeNode root){
+        if (root == null ){
+            return 0;
+        }
+        int leftDepth = maxDepthRecursive(root.left);
+        int rightDepth = maxDepthRecursive(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+    
+}
